991d7f18339ae47e02d30f998c27acc1c3dea38f
Jhonny Velasco <jvelascom5@est.ups.edu.ec>
2020-07-03 17:53:10 -0500

    Commit inicial

---
+++ ejemplo.py
@@ -0,0 +1,9 @@
+# Script de prueba para GitHub
+# Author: Jhonny Velasco
+# 27/06/2020
+def main():
+    nombre = input("Ingresa tu nombre:")
+    print('Hola',nombre,"Bienvenido a Git")
+
+if __name__ == "__main__":
+    main()
