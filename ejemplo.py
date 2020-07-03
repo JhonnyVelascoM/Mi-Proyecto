dac26b44ef3aa5ac12bd1a8c08dd7eea35c977ee
Jhonny Velasco <jvelascom5@est.ups.edu.ec>
2020-07-03 18:24:02 -0500

    Agregado datos del proyecto

--- ejemplo.py
+++ ejemplo.py
@@ -1,8 +1,9 @@
 # Script de prueba para GitHub
 # Author: Jhonny Velasco
 # 27/06/2020
+# Version 1.0.0
 def main():
-    nombre = input("Ingresa tu nombre:")
+    nombre = input(Ingresa tu nombre:")
     print('Hola',nombre,"Bienvenido a Git")
 
 if __name__ == "__main__":
